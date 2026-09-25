73944ed6ee2b44d252bf594e5601325951d85349
julioramosrgt2018-commits <julioramosrgt2018@gmail.com>
2026-09-25 11:52:16 -0300

    Implement service worker with caching
    Added service worker caching for PWA installation.

--- sw.js
+++ sw.js
@@ -1,3 +1,20 @@
-self.addEventListener('fetch', function(event) {
-  // Service Worker ativo para permitir instalação PWA
+const CACHE_NAME = 'compra-certa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
